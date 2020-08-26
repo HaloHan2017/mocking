b01a400453251e38991447ebd1f516cfbca58f0f
HaloHan2017 <2332722908@qq.com>
2020-08-26 17:21:37 +0800

    [mocking] [jerry,devin,andrea] test: implement test in VipParkingStrategyTest

--- src/test/java/parking/VipParkingStrategyTest.java
+++ src/test/java/parking/VipParkingStrategyTest.java
@@ -1,12 +1,23 @@
 package parking;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Collections;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class VipParkingStrategyTest {
+
+    @InjectMocks
+    private VipParkingStrategy vipParkingStrategy;
+    @Mock
+    private CarDao carDao;
 
     @Test
     public void testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName() {
@@ -30,6 +41,17 @@ public class VipParkingStrategyTest {
 
         /* Exercise 4, Write a test case on VipParkingStrategy.park()
          * With using Mockito spy, verify and doReturn */
+        //given
+        VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        Car car = mock(Car.class);
+        Receipt receipt = mock(Receipt.class);
+        doReturn(true).when(parkingLot).isFull();
+        //when
+        vipParkingStrategy.park(Collections.singletonList(parkingLot), car);
+        doReturn(receipt).when(vipParkingStrategy).park(Collections.singletonList(parkingLot), car);
+        //then
+        verify(vipParkingStrategy, times(1)).createNoSpaceReceipt(car);
     }
 
     @Test
@@ -39,6 +61,15 @@ public class VipParkingStrategyTest {
          * You may refactor the code, or try to use
          * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
          */
+        //given
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        Car car = mock(Car.class);
+        //when
+        doReturn(true).when(carDao).isVip(anyString());
+        doReturn("A").when(car).getName();
+        Receipt receipt = vipParkingStrategy.park(Collections.singletonList(parkingLot), car);
+        //then
+        assertNotNull(receipt);
     }
 
     @Test
@@ -48,6 +79,15 @@ public class VipParkingStrategyTest {
          * You may refactor the code, or try to use
          * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
          */
+        //given
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        Car car = mock(Car.class);
+        //when
+        doReturn(true).when(carDao).isVip(anyString());
+        doReturn("B").when(car).getName();
+        boolean isVip = vipParkingStrategy.isAllowOverPark(car);
+        //then
+        assertFalse(isVip);
     }
 
     @Test
@@ -56,6 +96,15 @@ public class VipParkingStrategyTest {
          * You may refactor the code, or try to use
          * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
          */
+        //given
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        Car car = mock(Car.class);
+        //when
+        doReturn(false).when(carDao).isVip(anyString());
+        doReturn("A").when(car).getName();
+        boolean isVip = vipParkingStrategy.isAllowOverPark(car);
+        //then
+        assertFalse(isVip);
     }
 
     @Test
@@ -64,6 +113,15 @@ public class VipParkingStrategyTest {
          * You may refactor the code, or try to use
          * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
          */
+        //given
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        Car car = mock(Car.class);
+        //when
+        doReturn(false).when(carDao).isVip(anyString());
+        doReturn("B").when(car).getName();
+        boolean isVip = vipParkingStrategy.isAllowOverPark(car);
+        //then
+        assertFalse(isVip);
     }
 
     private Car createMockCar(String carName) {
